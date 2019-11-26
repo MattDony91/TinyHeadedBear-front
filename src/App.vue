b71db1b2085a8b7fb5a9b1c510146bd38bf49279
hwakyung <hwakyoung05@gmail.com>
2019-11-26 09:56:47 +0900

    router, vuex 설치

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,12 @@
 <template>
   <div id="app">
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link class="navbar-brand" to="/">작은 대가리 곰</router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse nav" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <div v-if="isAuthenticated" class="row align-items-center">
-          <a href="#" @click.prevent="logout">logout</a>
-          <router-link class="nav-link" to="/mypage">mypage</router-link>
-        </div>
-        <div v-else>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/signup">SignUp</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/login">Login</router-link>
-          </li>
-        </div>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-  </nav>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -47,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
