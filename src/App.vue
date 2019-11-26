9db76bc580371c1b71e7b378fe7d5515e7d419cd
hwakyung <hwakyoung05@gmail.com>
2019-11-26 10:13:27 +0900

    저장 충돌 해결, css CDN 추가

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,90 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">작은 대가리 곰</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse nav" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <div v-if="isAuthenticated" class="row align-items-center">
+            <a href="#" @click.prevent="logout">logout</a>
+            <router-link class="nav-link" to="/mypage">mypage</router-link>
+          </div>
+          <div v-else>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/signup">SignUp</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+          </div>
+        </ul>
+        
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view/>
     </div>
-    <router-view/>
+    <!-- <Home :check="isAuthenticated"/> -->
   </div>
 </template>
+<script>
+// import Login from './views/Login.vue'
+// import Home from './views/Home.vue'
+export default {
+  name: 'App',
+  components:{
+    // Home,
+  },
+  data: function(){
+    return {
+      isAuthenticated: this.$session.has('jwt'),
+      // isAuthenticated: this.$store.getters.isAuthenticated
+    }
+  },
+  methods: {
+    logout: function(){
+      console.log('로그아웃버튼!')
+      this.$session.destroy()
+      // this.$store.dispatch('logout')
+      this.$router.push('/login') 
+    },
+  
+  },
+  updated: function(){
+    this.isAuthenticated =this.$session.has('jwt')
+    // this.isAuthenticated = this.$store.getters.isAuthenticated
+  }
+ 
+}
+</script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
-#nav {
+.nav {
   padding: 30px;
 }
 
-#nav a {
+.nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: whitesmoke;
 }
 
-#nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
